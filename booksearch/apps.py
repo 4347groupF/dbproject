b63513bca5e691bf2f61a4d7c715847d712951bd
Yong hyun Lee <leewqa0@gmail.com>
2023-11-10 18:50:57 -0600

    Delete booksearch directory

--- booksearch/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BooksearchConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "booksearch"
--- booksearch/urls.py
+++
@@ -1,8 +0,0 @@
-# booksearch/urls.py
-
-from django.urls import path
-from .views import BookListView
-
-urlpatterns = [
-    path('search/', BookListView.as_view(), name='book-list'),
-]
