3985d1dd2607a7f1730408c4fdc28bf63c87084f
Sailesh Andra <38543086+SaileshAndra@users.noreply.github.com>
2023-11-11 09:30:21 -0600

    move templates

--- booksearch/templates/booksearch/script.js
+++ booksearch/templates/booksearch/script.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
         event.preventDefault();
 
         var input1 = document.getElementById("userid").value.trim();
-        var input2 = document.getElementById("password").value.trim();
+       
 
 
-        if(input1 === "ABC200001" && input2 === "P4ssword@UTD") {
-            window.location.href = "home.html";
+        if(input1 === "ABC200001") {
+            window.location.href = "index.html";
         } else {
             document.getElementById("rejection").style.display = "block";
         }
