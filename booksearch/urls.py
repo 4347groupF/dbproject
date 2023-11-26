8a4dc860f873d0fafd0033e86ad15b8f338f95f9
DavidYang0 <dxy190013@utdallas.eduw>
2023-11-25 21:14:09 -0600

    Fix signup
    Linked to wrong function

--- booksearch/urls.py
+++ booksearch/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # Existing URL patterns
     path('', views.login_page, name='login_page'),
     path('login_fail/', views.login_validation, name='login_validation'),
-    path('signup/', views.signup_page, name='signup_page'),
+    path('signup/', views.signup, name='signup_page'),
     path('index/', views.home, name='index'),
     path('search/', views.search_books, name='search_books'),  
     path('loans/', views.loans, name='loan_search'),
