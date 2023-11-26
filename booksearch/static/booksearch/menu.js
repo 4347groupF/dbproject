60726aff338f5ce707392293d7cbf608d15f4d70
Yong hyun Lee <leewqa0@gmail.com>
2023-11-26 17:57:39 -0600

    Adding new directory for JS file and PNG files

---
+++ booksearch/static/booksearch/menu.js
@@ -0,0 +1,16 @@
+console.log('menu.js loaded');
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const menuList = document.getElementById("menu-list");
+
+    menuIcon.addEventListener("click", function () {
+        if (menuList.style.display === "block") {
+            menuList.style.display = "none";
+        } else {
+            menuList.style.display = "block";
+            menuList.style.position = "absolute";
+            menuList.style.top = menuIcon.getBoundingClientRect().bottom + "px";
+            menuList.style.right = "0"; // Adjust this if needed
+        }
+    });
+});
